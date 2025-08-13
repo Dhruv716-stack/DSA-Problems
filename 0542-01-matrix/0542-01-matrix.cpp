a8b870b9dbbf771cfcd788b6ef8629a5b983feee
Dhruv716-stack <sahudhruv02004@gmail.com>
2025-08-13 22:01:54 +0530

    Time: 21 ms (49.84%), Space: 34.8 MB (70.39%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int m = mat.size(), n = mat[0].size();
+        vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
+        queue<pair<int, int>> q;
+
+        // Step 1: Push all 0s into the queue
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 0) {
+                    dist[i][j] = 0;
+                    q.push({i, j});
+                }
+            }
+        }
+
+        // Directions: up, down, left, right
+        int dirs[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+        // Step 2: BFS
+        while (!q.empty()) {
+            auto [x, y] = q.front();
+            q.pop();
+
+            for (auto &dir : dirs) {
+                int nx = x + dir[0], ny = y + dir[1];
+                if (nx >= 0 && ny >= 0 && nx < m && ny < n) {
+                    if (dist[nx][ny] > dist[x][y] + 1) {
+                        dist[nx][ny] = dist[x][y] + 1;
+                        q.push({nx, ny});
+                    }
+                }
+            }
+        }
+
+        return dist;
+    }
+};
