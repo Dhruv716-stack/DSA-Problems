9aa294b5daec206c4746bde69968787f25ed66a8
Dhruv716-stack <sahudhruv02004@gmail.com>
2025-08-14 16:27:38 +0530

    Time: 0 ms (100%), Space: 14.4 MB (73.73%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,52 +1,45 @@
 class Solution {
 public:
+void dfs(vector<vector<char>>& board,int delr[],int delc[],vector<vector<int>>&vis,int r,int c)
+{
+    int m=board.size();
+    int n=board[0].size();
+    vis[r][c]=1;
+    for(int i=0;i<4;i++)
+    {
+        int nr=r+delr[i];
+        int nc=c+delc[i];
+        if(nr>=0 && nr<m && nc>=0 && nc<n && vis[nr][nc]==0 && board[nr][nc]=='O') dfs(board,delr,delc,vis,nr,nc);
+    }
+    return;
+}
     void solve(vector<vector<char>>& board) {
         int m=board.size();
         int n=board[0].size();
-        queue<pair<int,int>> q;
         vector<vector<int>> vis(m,vector<int>(n,0));
+        int delr[4]={-1,0,1,0};
+        int delc[4]={0,1,0,-1};
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
-                if(board[i][j]=='O')
+                if(i==0 || j==0 ||i==m-1 || j==n-1)
                 {
-                    if(i==0||i==m-1||j==0 || j==n-1)
+                    if(board[i][j]=='O')
                     {
                         vis[i][j]=1;
-                        q.push({i,j});
+                        dfs(board,delr,delc,vis,i,j);
                     }
                 }
-                
             }
         }
-        int delr[4]={-1,0,1,0};
-        int delc[4]={0,1,0,-1};
-        while(!q.empty())
+        for(int i=0;i<m;i++)
         {
-            auto p=q.front();
-            q.pop();
-            int r=p.first;
-            int c=p.second;
-            for(int i=0;i<4;i++)
+            for(int j=0;j<n;j++)
             {
-                int nr=r+delr[i];
-                int nc=c+delc[i];
-                if(nr>=0 && nr<m && nc>=0 && nc<n && board[nr][nc]=='O' && vis[nr][nc]==0)
-                {
-                    vis[nr][nc]=1;
-                    q.push({nr,nc});
-                }
+                if(board[i][j]=='O' && vis[i][j]==0) board[i][j]='X';
             }
         }
-            for(int i=0;i<m;i++)
-            {
-                for(int j=0;j<n;j++)
-                {
-                    if(board[i][j]=='O' && vis[i][j]==0) board[i][j]='X';
-                }
-            }
-        
         return;
     }
 };
