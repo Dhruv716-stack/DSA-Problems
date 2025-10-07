870babfe0696deae1080b2604e70c9d4f44a7298
Dhruv716-stack <sahudhruv02004@gmail.com>
2025-10-07 12:42:17 +0530

    Time: 0 ms (100%), Space: 12.1 MB (6.22%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int solve(int m,int n,int r,int c,vector<vector<int>>&dp,vector<vector<int>>&grid)
     {
-        if(grid[0][0]==1) return 0;
-        if(r==m-1 &&c==n-1 && grid[r][c]!=1) return 1;
+        if(grid[m-1][n-1]==1) return 0;
+        if(r==0 &&c==0 && grid[r][c]!=1) return 1;
         if(dp[r][c]!=-1) return dp[r][c];
         int l=0,ri=0;
         if(r>=0 && r<m && c>=0 && c<n)
         {
 
-            if(r+1<m && grid[r+1][c]!=1) l=solve(m,n,r+1,c,dp,grid);
-            if(c+1<n && grid[r][c+1]!=1) ri=solve(m,n,r,c+1,dp,grid);
+            if(r-1>=0 && grid[r-1][c]!=1) l=solve(m,n,r-1,c,dp,grid);
+            if(c-1>=0 && grid[r][c-1]!=1) ri=solve(m,n,r,c-1,dp,grid);
         }
 
         return dp[r][c]=l+ri;
@@ -19,7 +19,7 @@ public:
         int m=obstacleGrid.size();
         int n=obstacleGrid[0].size();
         vector<vector<int>> dp(m,vector<int>(n,-1));
-        int ans=solve(m,n,0,0,dp,obstacleGrid);
+        int ans=solve(m,n,m-1,n-1,dp,obstacleGrid);
         return ans;
     }
 };
