bd2c56e2716c26161132fca3493ec4823242da6b
Dhruv716-stack <sahudhruv02004@gmail.com>
2025-10-23 21:03:19 +0530

    Time: 0 ms (100%), Space: 15.4 MB (89.74%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -4,9 +4,10 @@ public:
         int m=grid.size();
         int n=grid[0].size();
         vector<int> prev(n,0);
+        vector<int> curr(n,0);
         for(int i=0;i<m;i++)
         {
-            vector<int> curr(n,0);
+            
             for(int j=0;j<n;j++)
             {
                 if(i==0 && j==0) curr[j]=grid[0][0];
