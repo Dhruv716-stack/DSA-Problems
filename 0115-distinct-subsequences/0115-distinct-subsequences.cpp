2874da3f38b1c547c217e5bd7b6f9523fecb28a6
Dhruv716-stack <sahudhruv02004@gmail.com>
2025-11-11 10:37:34 +0530

    Time: 39 ms (19.6%), Space: 65.6 MB (10.83%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,17 +1,22 @@
+using int128 = __int128;
+const int M=1e9+7;
 class Solution {
 public:
-    int solve(string &s, string &t, int i, int j, vector<vector<int>>&dp)
-    {
-        if(j<0) return 1;
-        if(i<0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]==t[j]) return dp[i][j]= (solve(s,t,i-1,j-1,dp)+solve(s,t,i-1,j,dp));
-        else return dp[i][j]=solve(s,t,i-1,j,dp);
-    }
     int numDistinct(string s, string t) {
         int n=s.size();
         int m=t.size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return solve(s,t,n-1,m-1,dp);
+        vector<vector<int128>> dp(n+1,vector<int128>(m+1,0));
+        for(int i=0;i<=n;i++) dp[i][0]=1;
+        for(int j=1;j<=m;j++) dp[0][j]=0;
+
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(s[i-1]==t[j-1]) dp[i][j]=(dp[i-1][j-1]+dp[i-1][j])%M;
+                else dp[i][j]=(dp[i-1][j])%M;
+            }
+        }
+        return (long long)(dp[n][m])%M;
     }
 };
