06dae0485c40f2facc83a122c97896baf9276eac
Dhruv716-stack <sahudhruv02004@gmail.com>
2025-11-11 14:06:39 +0530

    Time: 8 ms (46.09%), Space: 13.6 MB (27.19%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -19,10 +19,26 @@ public:
         string s2=word2;
         int n=s1.size();
         int m=s2.size();
-        vector<vector<int>> dp(n,vector<int>(m,INT_MAX));
-        int x=n-1;
-        int ans=solve(s1,s2,x,m-1,dp);
-        return ans;
+        vector<vector<int>> dp(n+1,vector<int>(m+1,INT_MAX));
+
+        for(int i=0;i<=n;i++) dp[i][0]=i;
+        for(int j=1;j<=m;j++) dp[0][j]=j;
+
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(s1[i-1]==s2[j-1]) dp[i][j]=dp[i-1][j-1];
+                else
+                {
+                    int del=1+dp[i-1][j];
+                    int ins=1+dp[i][j-1];
+                    int re=1+dp[i-1][j-1];
+                    dp[i][j]=min({del,ins,re});
+                }
+            }
+        }
+        return dp[n][m];
         
     }
 };
