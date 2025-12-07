9f15a97f5f720ff60269c3a8d75c623a90146808
Dhruv716-stack <sahudhruv02004@gmail.com>
2025-12-07 16:21:15 +0530

    Time: 1925 ms (10.25%), Space: 337.1 MB (6.8%) - LeetHub

---
+++ 3770-largest-prime-from-consecutive-prime-sum/3770-largest-prime-from-consecutive-prime-sum.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<bool> prime(int n)
+    {
+        vector<int> sum(n,0);
+        vector<bool> is_prime(n+1,1);
+        if(n>=0) is_prime[0]=0;
+        if(n>=1) is_prime[1]=0;
+        for(int i=2;i<=n;i++)
+        {
+            if(is_prime[i]==1)
+            {
+                for(int j=2*i;j<=n;j+=i)
+                {
+                    is_prime[j]=0;
+                }
+            }
+        }
+        return is_prime;
+    }
+    vector<long long> p_sum(vector<bool>&is_prime)
+    {
+        int n1=is_prime.size();
+        vector<long long>p(n1+1,0);
+        for(int i=2;i<=n1;i++)
+        {
+            p[i]=p[i-1];
+            if(is_prime[i-1]==1)
+            {
+                p[i]+=(i-1);
+            }
+        }
+        return p;
+    }
+    int largestPrime(int n) {
+        vector<bool> is_prime=prime(n);
+        vector<long long> prime_sum=p_sum(is_prime);
+        //if(is_prime[n]==1) return n;
+        int n1=prime_sum.size();
+        long long ans=0;
+        for(int i=0;i<n1;i++)
+        {
+            if( prime_sum[i]<=n && is_prime[prime_sum[i]]==1)
+            {
+                ans=max(ans,prime_sum[i]);
+            }
+        }
+        return ans;
+    }
+};
