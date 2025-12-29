6fa8fdabfdc0c4179ecfaceb9211d213c5d062d5
Dhruv716-stack <sahudhruv02004@gmail.com>
2025-12-29 19:41:54 +0530

    Time: 3 ms (85.01%), Space: 12.7 MB (9.06%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,7 +1,7 @@
 #define ll long long
 class Solution {
 public:
-    ll solve(int i, vector<int>&arr,vector<ll>&dp,int k)
+    /*ll solve(int i, vector<int>&arr,vector<ll>&dp,int k)
     {
         int n=arr.size();
         if(i==n) return 0;
@@ -17,10 +17,24 @@ public:
             max_ans=max(max_ans,sum);
         }
         return dp[i]=max_ans;
-    }
+    }*/
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n=arr.size();
-        vector<ll> dp(n,-1);
-        return solve(0,arr,dp,k);
+        vector<ll> dp(n+1,0);
+        ll max_ans=LLONG_MIN;
+        for(int i=n-1;i>=0;i--)
+        {
+            int len=0;
+            int maxi=INT_MIN;
+            for(int j=i;j<min(i+k,n);j++)
+            {
+                len++;
+                maxi=max(maxi,arr[j]);
+                ll sum=len*maxi*1LL+dp[j+1];
+                max_ans=max(max_ans,sum);
+            }
+            dp[i]=max_ans;
+        }
+        return dp[0];
     }
 };
