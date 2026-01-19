dcf3f3fad62232bc830da4a93b8ba8611a944bb6
Dhruv716-stack <sahudhruv02004@gmail.com>
2026-01-19 13:45:59 +0530

    Time: 1743 ms (15.76%), Space: 9.3 MB (84.62%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    bool is_palin(int i, int j, string &s)
+    bool is_palin(int i,int j,string &s)
     {
-        while(i<j)
+        while(i<=j)
         {
             if(s[i]!=s[j]) return false;
             i++;
@@ -10,42 +10,26 @@ public:
         }
         return true;
     }
-    /*int solve(int i, string &s,vector<int>&dp)
+    int solve(int i, string &s,vector<int> &dp)
     {
         int n=s.size();
         if(i==n) return 0;
         if(dp[i]!=-1) return dp[i];
-        int min_cost=INT_MAX;
+        int min_cost=1e7+3;
         for(int j=i;j<n;j++)
         {
-            int take=INT_MAX;
             if(is_palin(i,j,s)==true)
             {
                 int cost=1+solve(j+1,s,dp);
-                take=min(take,cost);
+                min_cost=min(min_cost,cost);
             }
-        min_cost=min({take,min_cost});
         }
         return dp[i]=min_cost;
-    }*/
+    }
     int minCut(string s) {
         int n=s.size();
-        vector<int> dp(n+1,0);
-        vector<int> front(n+1,0),curr(n+1,0);
-        for(int i=n-1;i>=0;i--)
-        {
-            int min_cost=INT_MAX;
-            for(int j=i;j<n;j++)
-            {
-                if(is_palin(i,j,s)==true)
-                {
-                    int cost=1+front[j+1];
-                    min_cost=min(min_cost,cost);
-                }
-            }
-            curr[i]=min_cost;
-            front=curr;
-        }
-        return front[0]-1;
+        vector<int> dp(n,-1);
+        return solve(0,s,dp)-1;
+
     }
 };
