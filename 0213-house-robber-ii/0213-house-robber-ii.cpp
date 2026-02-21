725dcdae89e3f0a26fe5609fc2e08a48d191c69e
Dhruv716-stack <sahudhruv02004@gmail.com>
2026-02-21 11:18:28 +0530

    Time: 0 ms (100%), Space: 10.4 MB (62.12%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
-        int rob1(vector<int>& nums) {
+    int solve(vector<int>&nums)
+    {
         int n=nums.size();
-        int prev1=nums[0];
-        int prev2=0;
+        int prev2=0,prev1=nums[0],curr=0;
         for(int i=1;i<n;i++)
         {
+            int not_take=prev1;
             int take=nums[i];
             if(i>1) take+=prev2;
-            int not_take=prev1;
-            int curri=max(take,not_take);
+            curr=max(not_take,take);
             prev2=prev1;
-            prev1=curri;
+            prev1=curr;
         }
         return prev1;
-        
     }
+    
     int rob(vector<int>& nums) {
         int n=nums.size();
         if(n==1) return nums[0];
-        vector<int> temp1;
-        vector<int> temp2;
+        if(n==0) return 0;
+        vector<int> temp1,temp2;
         for(int i=0;i<n;i++)
-            {
-                if(i!=0) temp1.push_back(nums[i]);
-                if(i!=n-1) temp2.push_back(nums[i]);
-            }
-        return max(rob1(temp1),rob1(temp2));
+        {
+            if(i!=0) temp1.push_back(nums[i]);
+            if(i!=n-1) temp2.push_back(nums[i]);
+        }
+
+        return max(solve(temp1),solve(temp2));
+
+
     }
 };
