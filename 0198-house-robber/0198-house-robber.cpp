6ca94eb34e85f1742da511fb1514913bba5341cd
Dhruv716-stack <sahudhruv02004@gmail.com>
2026-02-21 10:33:24 +0530

    Time: 0 ms (100%), Space: 10.9 MB (5.66%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -12,7 +12,17 @@ public:
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int> dp(n+1,-1);
-        return solve(n-1,nums,dp);
+        if(n == 0) return 0;
+        if(n == 1) return nums[0];
+        vector<long long> dp(n,0);
+        dp[0]=nums[0];
+        for(int i=1;i<n;i++)
+        {
+            long long not_take=dp[i-1];
+            long long take=1LL*nums[i];
+            if(i>1) take+=dp[i-2];
+            dp[i]=max(take,not_take);
+        }
+        return dp[n-1];
     }
 };
