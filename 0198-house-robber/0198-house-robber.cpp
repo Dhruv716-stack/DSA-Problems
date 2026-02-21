ac8584535b128dbb78657afbc5b798effdff1527
Dhruv716-stack <sahudhruv02004@gmail.com>
2026-02-21 10:18:46 +0530

    Time: 0 ms (100%), Space: 10.7 MB (44.65%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
+    int solve(int i, vector<int>&nums,vector<int>&dp)
+    {
+        if(i==0) return nums[i];
+        if(i<0) return 0;
+        if(dp[i]!=-1) return dp[i];
+        int not_take=solve(i-1,nums,dp);
+        int take=0;
+        if(i>=0) take=nums[i]+solve(i-2,nums,dp);
+        return dp[i]=max(not_take,take);
+    }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        int prev1=nums[0];
-        int prev2=0;
-        for(int i=1;i<n;i++)
-        {
-            int take=nums[i];
-            if(i>1) take+=prev2;
-            int not_take=prev1;
-            int curri=max(take,not_take);
-            prev2=prev1;
-            prev1=curri;
-        }
-        return prev1;
-        
+        vector<int> dp(n+1,-1);
+        return solve(n-1,nums,dp);
     }
 };
