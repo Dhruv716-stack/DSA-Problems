a31071051dc23f75b33b2019b8bf8559e3f09a7b
Dhruv716-stack <sahudhruv02004@gmail.com>
2026-02-24 19:20:40 +0530

    Time: 0 ms (100%), Space: 13.8 MB (5.1%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    int solve(int i, int j, vector<vector<int>>&tri,vector<vector<long long>>&dp)
-    {
-        int n=tri.size();
-        if(i==n-1)
-        {
-            if(j<=i) return tri[i][j];
-            return INT_MAX;
-        }
-        if(j>i) return INT_MAX;
-        if(dp[i][j]!=INT_MAX) return dp[i][j];
-        long long p1=INT_MAX,p2=INT_MAX;
-        p1=1LL*tri[i][j]+solve(i+1,j,tri,dp);
-        p2=1LL*tri[i][j]+solve(i+1,j+1,tri,dp);
-        return dp[i][j]=min(p1,p2);
-    }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-        vector<vector<long long>> dp(n,vector<long long>(n,INT_MAX));
-        return solve(0,0,triangle,dp);
-
+        vector<vector<long long>> dp(n+1,vector<long long>(n+1,INT_MAX));
+        for(int j=0;j<n;j++) dp[n-1][j]=triangle[n-1][j];
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=i;j>=0;j--)
+            {
+                if(i==n-1) continue;
+                long long p1=INT_MAX,p2=INT_MAX;
+                p1=1LL*triangle[i][j]+dp[i+1][j];
+                p2=1LL*triangle[i][j]+dp[i+1][j+1];
+                dp[i][j]=min(p1,p2);
+            }
+        }
+        return dp[0][0];
     }
 };
