6bf9264a4d45ce185d2de643fa5704de9d6b4fe4
Dhruv716-stack <sahudhruv02004@gmail.com>
2026-02-26 11:31:39 +0530

    Time: 4 ms (21.39%), Space: 15.7 MB (9.36%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,21 +1,5 @@
 class Solution {
 public:
-    long long solve(int i, int j, vector<vector<int>>&mat,vector<vector<long long>>&dp)
-    {
-        int n=mat.size();
-        if(i==n-1)
-        {
-            if(j<n && j>=0) return 1LL*mat[i][j];
-            return INT_MAX;
-        }
-        if(j>=n || j<0) return INT_MAX;
-        if(dp[i][j]!=INT_MAX) return dp[i][j];
-        long long p1=INT_MAX,p2=INT_MAX,p3=INT_MAX;
-        p1=1LL*mat[i][j]+solve(i+1,j+1,mat,dp);
-        p2=1LL*mat[i][j]+solve(i+1,j,mat,dp);
-        p3=1LL*mat[i][j]+solve(i+1,j-1,mat,dp);
-        return dp[i][j]=min({p1,p2,p3});
-    }
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n=matrix.size();
         vector<vector<long long>> dp(n+1,vector<long long>(n+1,1e7));
