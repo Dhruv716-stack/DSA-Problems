74ee8cb563127b89c0a82410189e0862d50c812c
Dhruv Sahu <sahudhruv02004@gmail.com>
2026-06-02 18:53:32 +0530

    Time: 0 ms (100%), Space: 20.6 MB (92.98%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,28 +3,40 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n=matrix.size();
         int m=matrix[0].size();
-        vector<int> r(n,0);
-        vector<int> c(m,0);
+        int col0=1;
+        //row array--> matrix[0][..]
+        //col array--> matrix[..][0]
+
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 if(matrix[i][j]==0)
                 {
-                    r[i]=1;
-                    c[j]=1;
+                    if(j!=0) matrix[0][j]=0;
+                    if(j==0) col0=0;
+                    matrix[i][0]=0; //set ith col elemnt
                 }
             }
         }
 
-        for(int i=0;i<n;i++)
+        for(int i=1;i<n;i++)
         {
-        for(int j=0;j<m;j++)
-        {
-            if(r[i]==1 || c[j]==1) matrix[i][j]=0;
+            for(int j=1;j<m;j++)
+            {
+                if(matrix[i][0]==0 || matrix[0][j]==0) matrix[i][j]=0;
+            }
         }
+
+        if(matrix[0][0]==0)
+        {
+            for(int j=1;j<m;j++) matrix[0][j]=0;
+        }
+
+        if(col0==0)
+        {
+            for(int i=0;i<n;i++) matrix[i][0]=0;
         }
         return;
     }
-    
 };
