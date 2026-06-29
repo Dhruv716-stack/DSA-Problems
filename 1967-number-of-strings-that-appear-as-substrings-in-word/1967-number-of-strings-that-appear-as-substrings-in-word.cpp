380899252f7748a1583aea03d58900a7b4680e9b
Dhruv Sahu <sahudhruv02004@gmail.com>
2026-06-29 19:34:38 +0530

    Time: 0 ms (100%), Space: 12.8 MB (10.44%) - LeetHub

--- 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.cpp
+++ 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.cpp
@@ -1,31 +1,48 @@
 class Solution {
 public:
+    vector<int> lps_code(string &s)
+    {
+        int n=s.size();
+        vector<int> lps(n,0);
+        int len=0;
+        int i=1;
+        while(i<n){
+            if(s[i]==s[len]){
+                len++;
+                lps[i]=len;
+                i++;
+            }
+            else{
+                if(len!=0) len=lps[len-1];
+                else lps[i]=0;
+                i++;
+            }
+        }
+        return lps;
+    }
     int numOfStrings(vector<string>& patterns, string word) {
         int n=word.size();
         int ans=0;
         for(auto &w:patterns){
-            int sz=w.size();
-            
-            int j=0;
-            for(int i=0;i<n;i++){
-                if(word[i]==w[j]){
-                    int k=i;
-                    while(k<n && j<sz){
-                        if(word[k]==w[j]){
-                            k++;
-                            j++;
-                        }
-                        else{
-                            j=0;
-                            break;
-                        }
-                    }
+            int m=w.size();
+            vector<int> lps=lps_code(w);
 
-                    if(j==sz) ans++;
+            int i=0;
+            int j=0;
+            while(i<n && j<m){
+                if(w[j]==word[i]){
+                    i++;
+                    j++;
+                }
+
+                else if(w[j]!=word[i]){
+                    if(j!=0) j=lps[j-1];
+                    else i++;
                 }
             }
-        }
 
+            if(j==m) ans++;
+        }
         return ans;
     }
 };
