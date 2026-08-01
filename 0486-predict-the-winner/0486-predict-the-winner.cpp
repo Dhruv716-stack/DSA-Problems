4b167dc73bbd44fa920a2c784164aa264a973ec4
Dhruv Sahu <sahudhruv02004@gmail.com>
2026-08-01 11:20:23 +0530

    Time: 3 ms (39.21%), Space: 10.3 MB (32.54%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -6,8 +6,8 @@ public:
         int n=nums.size();
         if(i>j) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        ll take_i=1LL*nums[i]+min(solve(i+2,j,nums),solve(i+1,j-1,nums));
-        ll take_j=1LL*nums[j]+min(solve(i,j-2,nums),solve(i+1,j-1,nums));
+        ll take_i=1LL*nums[i]-solve(i+1,j,nums);
+        ll take_j=1LL*nums[j]-solve(i,j-1,nums);
         return dp[i][j]=max(take_i,take_j);
     } 
     bool predictTheWinner(vector<int>& nums) {
@@ -15,8 +15,7 @@ public:
         ll sum=0;
         for(int i=0;i<n;i++) sum+=nums[i];
         dp.assign(n,vector<ll>(n,-1));
-        ll p1=solve(0,n-1,nums);
-        ll p2=sum-p1;
-        return p1>=p2;
+        ll p=solve(0,n-1,nums);
+        return p>=0;
     }
 };
