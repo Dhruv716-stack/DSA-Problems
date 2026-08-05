7afad38a3fcd90185995cf5a23173543f91af399
Dhruv Sahu <sahudhruv02004@gmail.com>
2026-08-05 11:00:34 +0530

    Time: 0 ms (100%), Space: 10.7 MB (24.31%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,30 +1,18 @@
 class Solution {
 public:
-    int solve(int i, vector<int>&nums,vector<int>&dp)
-    {
-        if(i==0) return nums[i];
-        if(i<0) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int not_take=solve(i-1,nums,dp);
-        int take=0;
-        if(i>=0) take=nums[i]+solve(i-2,nums,dp);
-        return dp[i]=max(not_take,take);
+    vector<int> dp;
+    int solve(int ind, vector<int>&nums){
+        int n=nums.size();
+        if(ind>=n) return 0;
+        if(dp[ind]!=-1) return dp[ind];
+        int take=0,not_take=0;
+        not_take=solve(ind+1,nums);
+        take=nums[ind]+solve(ind+2,nums);
+        return dp[ind]=max(take,not_take);
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        if(n == 0) return 0;
-        if(n == 1) return nums[0];
-        //vector<long long> dp(n,0);
-        int prev2=0,prev1=nums[0],curr=0;
-        for(int i=1;i<n;i++)
-        {
-            long long not_take=prev1;
-            long long take=1LL*nums[i];
-            if(i>1) take+=prev2;
-            curr=max(take,not_take);
-            prev2=prev1;
-            prev1=curr;
-        }
-        return prev1;
+        dp.assign(n,-1);
+        return solve(0,nums);
     }
 };
