0354209fce58edca318c8a6a6f2e44aeaedd8b88
Dhruv Sahu <sahudhruv02004@gmail.com>
2026-09-11 13:24:22 +0530

    Time: 49 ms (79.51%), Space: 21.4 MB (41.18%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,64 +1,30 @@
 class Solution {
 public:
-    int diff(string &s1, string &s2){
-        int n=s1.size();
-        int cnt=0;
-        for(int i=0;i<n;i++){
-            if(s1[i]!=s2[i]){
-                cnt++;
-            }
-        }
-        return cnt;
-    }
-
-    unordered_map<string,vector<string>> make_adj(vector<string> &v){
-        int n=v.size();
-        int m=v[0].size();
-        unordered_map<string,vector<string>> mp;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                int cnt=diff(v[i],v[j]);
-                if(cnt==1){
-                    mp[v[i]].push_back(v[j]);
-                    mp[v[j]].push_back(v[i]);
-                }
-            }
-        }
-
-        return mp;
-    }
-
-    int bfs(string snode, unordered_map<string,int> &vis, unordered_map<string,vector<string>>&adj, string des){
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> st(wordList.begin(),wordList.end());
         queue<pair<string,int>> q;
-        q.push({snode,1});
-        vis[snode]=1;
+        q.push({beginWord,1});
+        if(st.find(beginWord)!=st.end()) st.erase(beginWord);
 
         while(!q.empty()){
-            string node=q.front().first;
-            int d=q.front().second;
-
+            auto [word,d]=q.front();
             q.pop();
-            if(node==des) return d;
 
-            for(auto &it:adj[node]){
-                if(vis.find(it)==vis.end()){
-                    q.push({it,d+1});
-                    vis[it]=1;
+            if(word==endWord) return d;
+
+            for(int i=0;i<word.size();i++){
+                char original=word[i];
+                for(char ch='a';ch<='z';ch++){
+                    word[i]=ch;
+                    if(st.find(word)!=st.end()){
+                        q.push({word,d+1});
+                        st.erase(word);
+                    }
                 }
+                word[i]=original;
             }
         }
 
         return 0;
-    }
-    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        wordList.push_back(beginWord);
-        unordered_map<string,vector<string>> adj;
-        adj=make_adj(wordList);
-
-        unordered_map<string,int> vis;
-        
-        int ans=bfs(beginWord,vis,adj,endWord);
-        return ans;
-
     }
 };
